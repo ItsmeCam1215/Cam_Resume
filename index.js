0eece10105146d21a2598914974528b521a59f28
Cam Nguyen <camnguyen1215@gmail.com>
2023-02-10 18:36:57 -0800

    Chat GPT

--- index.js
+++
@@ -1,9 +0,0 @@
-function colorChange(e) {
-    if ( e.target.style.color == "red") {
-    e.target.style.color = "blue"
-    e.target.innerHTML = "Hello World!"
-    } else {
-        e.target.style.color = "red"
-        e.target.innerHTML = "Here is my resume"
-    }
-}
